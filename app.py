f86abbbb65ca3a305ae08458e16d36214872bbab
Max Kelly <max.diamond.kelly@gmail.com>
2021-08-30 13:49:40 +1000

    forgot to add this file

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='frontend/build',static_url_path='')
+cors = CORS(app)
 
 from api.general.routes import general
 app.register_blueprint(general)
 
-# @app.route("/"):
-# 	def index():
-# 		return ""
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
 	app.run()
